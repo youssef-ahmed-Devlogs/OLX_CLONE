11b27f6fa6d8004d787f8bbff2f2ea5e82be42ba
youssefahmeddevlogs <youssefahmedsup@gmail.com>
2022-02-05 13:30:56 +0200

    header updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-// Start
+// START HEADER SECTION
+const userSettings = document.querySelector(".user-settings");
+const userIcon = document.querySelector(".user-icon");
+const userSettingsList = document.querySelector(".user-settings-list");
+let isOpen = false;
+
+userIcon.addEventListener("click", () => {
+  if (isOpen) {
+    userSettingsList.style.display = "none";
+    isOpen = false;
+  } else {
+    userSettingsList.style.display = "block";
+    isOpen = true;
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == userIcon) {
+    console.log("OK");
+  } else {
+    console.log("No");
+  }
+});
+// END HEADER SECTION
