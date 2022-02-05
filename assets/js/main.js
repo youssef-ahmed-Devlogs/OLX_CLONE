d475943deaa8cfbc31697ed4baa2c58a8b09fda5
abdoecomerce <abdoelprince12345@gmail.com>
2022-02-05 03:10:34 -0800

    finish the detils page by abdo el prince

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 // Start
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
